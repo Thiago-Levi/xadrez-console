d8a824821e05ab4a19418e7a57b9bcdd2ec02bdd
Thiago-Levi <thiago.levi@outlook.com>
2024-04-03 09:23:45 -0300

    Criando a classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using xadrez_console.Tabuleiro;
+
+Posicao p = new Posicao(3, 4);
+
+Console.WriteLine(p);
+
