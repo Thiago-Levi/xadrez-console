8fc1cede279d4c401a709c020418afef2ada0fe6
Thiago-Levi <thiago.levi@outlook.com>
2024-04-03 09:52:42 -0300

    Começando a implementar tabuleiro

---
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.Tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor {  get; set; }
+        public int QuantidadeDeMovimentos { get; set; }
+        public Tabuleiro Tabuleiro { get; set; }
+
+        public Peca(Posicao posicao, Tabuleiro tabuleiro, Cor cor) 
+        { 
+            Posicao = posicao;
+            Tabuleiro = tabuleiro;
+            Cor = cor;
+            QuantidadeDeMovimentos = 0;
+        }
+        
+
+
+    }
+}
---
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.Tabuleiro
+{
+    internal class Tabuleiro
+    {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] Pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            Pecas = new Peca[Linhas, Colunas]; 
+        }
+
+    }
+}
