cc8953b4436d476645f296e183ad29d2f362fd35
Thiago-Levi <thiago.levi@outlook.com>
2024-04-04 14:12:54 -0300

    Criando a classe PosicaoXadrez.

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -4,11 +4,6 @@ using xadrez;
 using tabuleiro;
 using xadrez_console;
 
-Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
-
-tabuleiro.InserirPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0,0));
-tabuleiro.InserirPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
-tabuleiro.InserirPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
-
-Tela.ImprimirTabuleiro(tabuleiro);
+PosicaoXadrez posicaoXadrez = new PosicaoXadrez('c', 7);
+Console.WriteLine(posicaoXadrez.ToPosicao());
--- xadrez-console/Tabuleiro/Tabuleiro.cs
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+
 
 
 namespace tabuleiro
@@ -20,12 +22,47 @@ namespace tabuleiro
         {
             return pecas[linha, coluna];
         }
+        
+        public Peca ObterPeca(Posicao posicao)
+        {
+            return pecas[posicao.linha, posicao.coluna];
+        }
+
+        public bool ExistePeca(Posicao posicao)
+        {
+            ValidarPosicao(posicao);
+            return ObterPeca(posicao) != null;
+        }
+
 
         public void InserirPeca(Peca peca, Posicao posicao)
         {
+            if (ExistePeca(posicao))
+            {
+                throw new TabuleiroException($"Já existe uma peça nessa posição (posição -> linha: {posicao.linha} coluna: {posicao.coluna})");
+            }
+
             pecas[posicao.linha, posicao.coluna] = peca;
             peca.posicao = posicao;
         }
 
+        public bool PosicaoValida(Posicao posicao)
+        {
+            if (posicao.linha < 0 || posicao.linha >= linhas || posicao.coluna < 0 || posicao.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao posicao)
+        {
+            if (!PosicaoValida(posicao))
+            {
+                throw new TabuleiroException($"Posição inválida! (posição -> linha: {posicao.linha} coluna: {posicao.coluna})");
+            }
+        }
+        
+    
     }
 }
